147ab798f228bc4bcead8816eaa32f9138703d4a
bauen1 <j2468h@gmail.com>
2020-04-17 12:25:54 +0200

    libruntime: implement simple exit

---
+++ libruntime/exit.s
@@ -0,0 +1,7 @@
+.p816
+.a16
+.i16
+
+.export _exit
+_exit:
+    BRK
---
+++ libruntime/include/stdlib.h
@@ -0,0 +1,9 @@
+#ifndef __STDLIB_H
+#define __STDLIB_H 1
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+extern void exit(int status);
+
+#endif
